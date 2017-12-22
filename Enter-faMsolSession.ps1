39e3d2787cc5543f18025c044ee30d23b1c09556
Mike <34536444+CReamflourish@users.noreply.github.com>
2017-12-22 09:20:15 +0000

    Initial commit of script for connecting to tenant. Tenant url's hard-coded. Synopsis and Descriptions added to comment-based help.

---
+++ Enter-faMsolSession.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+Connects to Microsoft O365 Online Servcies for tenant fletchersonline.onmicrosoft.com
+.DESCRIPTION
+Hard-coded script to connect to O365 online services for fletchersonline.onmicrosoft.com tenant. 
+
+Connects to Azure Active Directory, SharePoint Online, Skype for Business and Exchange Online and Security & Compliance Centre.
+#>
+
+# Create credentials object
+$credential = Get-Credential
+
+# Connect to Office 365
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+# Verification: Get-MsolDomain
+
+# Connect to SharePoint Online
+Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+Connect-SPOService -Url https://fletchersonline-admin.sharepoint.com -Credential $credential
+#Verification: Get-SPOSite
+
+# Connect to Skype for Business Online
+Import-Module SkypeOnlineConnector
+$sfboSession = New-CsOnlineSessioN -Credential $credential
+Import-PSSession $sfboSession
+
+# Connect to Exchange Online
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid" -Credential $credential -Authentication Basic -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+# Verification: Get-AcceptedDomain | Format-List -Property DomainName
+
+# Connect to Security & Compliance Centre
+$ccSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRedirection
+Import-PSSession $ccSession -Prefix cc
+
+Write-Host "Connected to Office 365 Services" -ForegroundColor Green
