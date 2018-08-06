8516d9265f4447f6eab613951eb6a3647d461a3d
Mike Kirby <mike.kirby@stafford-house.com>
2018-08-06 09:34:28 +0100

    Remove call to .\Connect-faEXOPSSession.ps1

--- secure_score/Enable-Mailboxauditing.ps1
+++ secure_score/Enable-Mailboxauditing.ps1
@@ -1,7 +1,5 @@
 #This script will enable non-owner mailbox access auditing on every mailbox in your tenancy
 
-.\Connect-faEXOPSSession.ps1
-
 #Enable global audit logging
 Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox" -or RecipientTypeDetails -eq "SharedMailbox" -or RecipientTypeDetails -eq "RoomMailbox" -or RecipientTypeDetails -eq "DiscoveryMailbox"} | Set-Mailbox -AuditEnabled $true -AuditLogAgeLimit 180 -AuditAdmin Update, MoveToDeletedItems, SoftDelete, HardDelete, SendAs, SendOnBehalf, Create, UpdateFolderPermission -AuditDelegate Update, SoftDelete, HardDelete, SendAs, Create, UpdateFolderPermissions, MoveToDeletedItems, SendOnBehalf -AuditOwner UpdateFolderPermission, MailboxLogin, Create, SoftDelete, HardDelete, Update, MoveToDeletedItems 
 
