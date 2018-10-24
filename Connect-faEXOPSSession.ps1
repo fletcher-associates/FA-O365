45807ef0f4315b29efda1900243bb867431458d2
Mike Kirby <mike.kirby@fletcher-associates.co.uk>
2018-10-24 10:51:34 +0100

    Fix to retain current location after run

--- Connect-faEXOPSSession.ps1
+++ Connect-faEXOPSSession.ps1
@@ -13,6 +13,7 @@ Give a usage example with explanation.
 (repeat as required)
 #>
 
+$location = Get-Location
 $modules = @(Get-ChildItem -Path "$($env:LOCALAPPDATA)\Apps\2.0" -Filter "Microsoft.Exchange.Management.ExoPowershellModule.manifest" -Recurse )
 $moduleName =  Join-Path $modules[0].Directory.FullName "Microsoft.Exchange.Management.ExoPowershellModule.dll"
 Import-Module -FullyQualifiedName $moduleName -Force
@@ -20,3 +21,4 @@ $scriptName =  Join-Path $modules[0].Directory.FullName "CreateExoPSSession.ps1"
 . $scriptName
 $null = Connect-EXOPSSession
 $exchangeOnlineSession = (Get-PSSession | Where-Object { ($_.ConfigurationName -eq 'Microsoft.Exchange') -and ($_.State -eq 'Opened') })[0]
+Set-Location $location
