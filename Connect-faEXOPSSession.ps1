c9e25e38b6f860793dcbecd270cabe98eea73563
CReamflourish <34536444+CReamflourish@users.noreply.github.com>
2019-01-14 16:16:47 +0000

    Updates to make an advanced function

--- Connect-faEXOPSSession.ps1
+++ Connect-faEXOPSSession.ps1
@@ -1,24 +1,37 @@
-<#
-.SYNOPSIS
-Connects to Exchange Online service with MFA from console (without needing to use the Exchange Online Shell).
-.DESCRIPTION
-Give a more detailed and fuller description of what it does. Elaborate on usage scenarios, limitations, prerequisites and cross-refer to other similar scripts as necessary where there could be confusion.
-.PARAMETER ParameterName
-Give details of parameter and how its used.
-.PARAMETER ParameterName
-(repeat as required)
-.EXAMPLE
-Give a usage example with explanation.
-.EXAMPLE
-(repeat as required)
-#>
+function Connect-faEXOPSSession{
 
-$location = Get-Location
-$modules = @(Get-ChildItem -Path "$($env:LOCALAPPDATA)\Apps\2.0" -Filter "Microsoft.Exchange.Management.ExoPowershellModule.manifest" -Recurse )
-$moduleName =  Join-Path $modules[0].Directory.FullName "Microsoft.Exchange.Management.ExoPowershellModule.dll"
-Import-Module -FullyQualifiedName $moduleName -Force
-$scriptName =  Join-Path $modules[0].Directory.FullName "CreateExoPSSession.ps1"
-. $scriptName
-$null = Connect-EXOPSSession
-$exchangeOnlineSession = (Get-PSSession | Where-Object { ($_.ConfigurationName -eq 'Microsoft.Exchange') -and ($_.State -eq 'Opened') })[0]
-Set-Location $location
+    <#
+    .SYNOPSIS
+    Connects to Exchange Online service using MFA.
+    .DESCRIPTION
+    Connects to Exchange Online service using MFA. Allows connections from the console without needing to use the Exchange Online Shell.
+    .EXAMPLE
+    Connect-faEXOPSSession
+
+    A separate login dialog will open for you to enter your Office 365 credentials.
+    #>
+
+[CmdletBinding()]
+Param()
+
+    BEGIN{
+        $location = Get-Location
+    }#begin
+
+    PROCESS{
+
+        $modules = @(Get-ChildItem -Path "$($env:LOCALAPPDATA)\Apps\2.0" -Filter "Microsoft.Exchange.Management.ExoPowershellModule.manifest" -Recurse )
+        $moduleName =  Join-Path $modules[0].Directory.FullName "Microsoft.Exchange.Management.ExoPowershellModule.dll"
+        Import-Module -FullyQualifiedName $moduleName -Force
+        $scriptName =  Join-Path $modules[0].Directory.FullName "CreateExoPSSession.ps1"
+        . $scriptName
+        $null = Connect-EXOPSSession
+        $exchangeOnlineSession = (Get-PSSession | Where-Object { ($_.ConfigurationName -eq 'Microsoft.Exchange') -and ($_.State -eq 'Opened') })[0]
+        Write-Output $exchangeOnlineSession
+    }#process
+
+    END{
+        Set-Location $location
+    }#end
+
+}#function
