27b2c1b2b7fc9c54fbe352c827e284ecfe961699
CReamflourish <34536444+CReamflourish@users.noreply.github.com>
2019-02-01 16:14:09 +0000

    Removed comments for test run

--- _WorkInProgress/_AddExternalUserGrouptoSite.ps1
+++ _WorkInProgress/_AddExternalUserGrouptoSite.ps1
@@ -4,15 +4,15 @@ $sites = Get-SPOSite -Filter { Url -like "https://fletchersonline.sharepoint.com
 foreach ($site in $sites) {
 
     Write-Host "Connecting to $($site.Url)"
-    #Connect-PnPOnline -Url $site.Url -UseWebLogin
+    Connect-PnPOnline -Url $site.Url -UseWebLogin
 
     Write-Host "Adding new permission level to $($site.Title)"
-    #Add-PnPRoleDefinition -RoleName 'Limited Read' -Clone Read -Exclude ViewVersions -Description 'Based on Read but excludes ViewVersions'
+    Add-PnPRoleDefinition -RoleName 'Limited Read' -Clone Read -Exclude ViewVersions -Description 'Based on Read but excludes ViewVersions'
 
     Write-Host 'Adding new site group'
-    #New-PnPGroup -Title "$($site.Title) Client Users" -Description 'Group for Client users'
+    New-PnPGroup -Title "$($site.Title) Client Users" -Description 'Group for Client users'
 
     Write-Host 'Assigning permission level to site group'
-    #Set-PnPGroupPermissions -Identity "$($site.Title) Client Users" -AddRole 'Limited Read'
+    Set-PnPGroupPermissions -Identity "$($site.Title) Client Users" -AddRole 'Limited Read'
 
 }#foreach
